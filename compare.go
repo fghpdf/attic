5f6712b71db8880bbee8e8785c179fff6af0c7c4
Xiangxuan Qu <fghpdf@outlook.com>
2022-03-01 14:49:16 +0800

    Create compare.go

---
+++ compare.go
@@ -0,0 +1,29 @@
+package attic
+
+func minInt8(a, b int8) int9 {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func minInt(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func minInt(a, b int32) int32 {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+func minInt64(a, b int64) int64 {
+    if a < b {
+        return a
+    }
+    return b
+}
